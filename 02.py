1e3d2eb2929f248c4c41b4edd442dfaf97060370
Chi <cychoi@zoho.com>
2024-12-02 09:08:13 -0500

    Finish day 2

---
+++ 02.py
@@ -0,0 +1,64 @@
+from lib import read_input, timer
+
+
+def parse_reports():
+    reports = []
+    for line in read_input(2):
+        report = [int(n) for n in line.split(' ')]
+        reports.append(report)
+    return reports
+
+
+def report_intervals(report):
+    return [n - report[i - 1] for i, n in enumerate(report) if i > 0]
+
+
+def is_safe(intervals, min_step, max_step):
+    safely_increases = all(
+        (min_step <= step and step <= max_step for step in intervals)
+    )
+    safely_decreases = all(
+        (-max_step <= step and step <= -min_step for step in intervals)
+    )
+    return safely_increases or safely_decreases
+
+
+def count_safe_reports(reports, min_step, max_step):
+    return sum(
+        (
+            is_safe(report_intervals(report), min_step, max_step)
+            for report in reports
+        )
+    )
+
+
+def count_safe_reports_dampened(reports, min_step, max_step):
+    safe_count = 0
+    for report in reports:
+        report_variations = (
+            report[0:i] + report[i + 1 : len(report)]
+            for i in range(len(report))
+        )
+        if any(
+            (
+                is_safe(report_intervals(variation), min_step, max_step)
+                for variation in report_variations
+            )
+        ):
+            safe_count += 1
+
+    return safe_count
+
+
+if __name__ == '__main__':
+    reports = parse_reports()
+
+    print('Day 2, Part 1')
+    with timer():
+        result = count_safe_reports(reports, min_step=1, max_step=3)
+    print(f'Safe reports: {result}\n')  # 314
+
+    print('Day 2, Part 2')
+    with timer():
+        result = count_safe_reports_dampened(reports, min_step=1, max_step=3)
+    print(f'Safe reports: {result}\n')  # 373
